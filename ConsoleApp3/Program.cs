19fa7bc26b1d20d12c33164309717f0cbabadcff
César Tapia <c.tapia@korvi.net>
2018-10-05 17:44:02 -0500

    Learning conditionals

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            int value1 = 0, value2 = 0;
+            string chain;
+            Console.WriteLine("Give me the first value:");
+            chain = Console.ReadLine();
+            value1 = Convert.ToInt32(chain);
+            Console.WriteLine("Give me the second value:");
+            chain = Console.ReadLine();
+            value2 = Convert.ToInt32(chain);
+            if (value1 <= value2)
+            {
+                if (value1 == value2)
+                    Console.WriteLine("The value of 1 is equal to value of 2");
+                else
+                    Console.WriteLine("The value of 2 is greater than value of 1");
+            }
+            else
+                Console.WriteLine("The value of 1 is grater than the value of 2");
+            Console.ReadKey();
         }
     }
 }
